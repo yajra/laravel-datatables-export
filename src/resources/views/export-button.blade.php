5f24bf88d57a6ada2496d08f19f9598fc7da0ad0
Arjay Angeles <aqangeles@gmail.com>
2021-10-04 15:52:17 +0800

    Add ajax error handler.

--- src/resources/views/export-button.blade.php
+++ src/resources/views/export-button.blade.php
@@ -5,6 +5,10 @@
                 var url = window._buildUrl(LaravelDataTables['{{ $tableId }}'], 'exportQueue');
                 $.get(url + '&exportType={{$type}}').then(function(exportId) {
                     $wire.export(exportId)
+                }).catch(function(error) {
+                    $wire.exportFinished = true;
+                    $wire.exporting = false;
+                    $wire.exportFailed = true;
                 });
               "
     >
