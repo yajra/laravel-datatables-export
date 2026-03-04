d672b6d64a1f8fa5fd181e5afc47932fd4d32e07
Sébastien Alfaiate <s.alfaiate@webarea.fr>
2026-03-04 16:20:53 +0700

    fix: Do not replace empty values by null

--- src/Jobs/DataTableExportJob.php
+++ src/Jobs/DataTableExportJob.php
@@ -141,7 +141,7 @@ class DataTableExportJob implements ShouldBeUnique, ShouldQueue
                 }
 
                 /** @var array|bool|int|string|null|DateTimeInterface $value */
-                $value = $this->getValue($row, $property) ?? '';
+                $value = $this->getValue($row, $property);
 
                 if (isset($column->exportRender)) {
                     $callback = $column->exportRender;
